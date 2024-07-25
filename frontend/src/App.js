0675002d4f3aef1f8adeecc35f95acd4192676e5
EduardaFarias <maria.eduarda.farias@ccc.ufcg.edu.br>
2024-07-25 08:49:21 -0300

    feat: implementando o home com react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AppBar  from "./components/AppBar"
+import Home from "./screens/Home"
 
 function App() {
   return (
     <div className="App">
       <AppBar></AppBar>
+      <Home></Home>
     </div>
   );
 }
